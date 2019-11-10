7d344b8df17ec4a807a449777668e7cd865e3e8b
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2019-11-10 18:36:43 -0300

    Creada clase Platforms

--- src/gameElements/level.cpp
+++ src/gameElements/level.cpp
@@ -1,1 +1,44 @@
 #include "level.h"
+
+Platform::Platform()
+{
+	rectangle.setSize({ 200, 30 });
+	rectangle.setPosition({ 100, 100 });
+	rectangle.setFillColor(Color::White);
+}
+
+Platform::Platform(float x, float y, float width, float height, Color color)
+{
+	rectangle.setSize({ width,height });
+	rectangle.setPosition({ x, y });
+	rectangle.setFillColor(color);
+}
+
+Platform::~Platform()
+{
+}
+
+RectangleShape Platform::getRec()
+{
+	return rectangle;
+}
+
+int Platform::bottomSide()
+{
+	return rectangle.getPosition().y + rectangle.getSize().y;
+}
+
+int Platform::upperSide()
+{
+	return rectangle.getPosition().y;
+}
+
+int Platform::rightSide()
+{
+	return rectangle.getPosition().x + rectangle.getSize().x;
+}
+
+int Platform::leftSide()
+{
+	return rectangle.getPosition().x;
+}
--- src/gameElements/level.h
+++ src/gameElements/level.h
@@ -1,23 +1,24 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
 class Platform
 {
 private:
-
+	RectangleShape rectangle;
 public:
 	Platform();
+	Platform(float x, float y, float width, float height, Color color);
 	~Platform();
-
+	RectangleShape getRec();
+	int bottomSide();
+	int upperSide();
+	int rightSide();
+	int leftSide();
 };
-
-Platform::Platform()
-{
-}
-
-Platform::~Platform()
-{
-}
 
 #endif // !LEVEL_H
 
