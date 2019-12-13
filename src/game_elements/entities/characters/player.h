f2649772afc8834fb2b63f06820b385b138f7d39
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2019-12-13 07:54:43 -0300

    Arreglada gravedad

--- src/game_elements/entities/characters/player.h
+++ src/game_elements/entities/characters/player.h
@@ -36,7 +36,7 @@ class Player : public Character
 	Vector2f INITIAL_POS = { 3100.0f, -1500.0f };
 	const int SPRITE_SIZE = 128;
 	const int SPRITE_Y_SETOFF = 30;
-	const float MAXIMUM_SPEED = 1200.0f;//20.0f;
+	const float MAXIMUM_SPEED = 1200.0f;
 	const float MOVING_SPEED = 600.0f;
 	const float COLLISION_MARGIN = 7.0f;
 	Movement movement;
